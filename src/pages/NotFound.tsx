c73c794e17f347a069168368676009f20082a7b9
ThallesLana <thalles.lana1@gmail.com>
2026-01-04 20:34:18 -0300

    :sparkles: feat: add favicon and change notFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Button } from "../components/ui/button";
 
 const NotFound = () => {
   const location = useLocation();
@@ -11,11 +12,13 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
       <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-muted-foreground">Oops! Page not found</p>
-        <a href="/" className="text-primary underline hover:text-primary/90">
-          Return to Home
-        </a>
+        <img src="src/assets/404.svg" alt="Not Found" className="w-72 h-72 mb-4 self-center" />
+        <p className="text-center mb-5">Uai, encontramo esse trem não, sô!</p>
+        <Button variant="hero" className="text-white" asChild>
+          <a href="/">
+            Voltar para a Home
+          </a>
+        </Button>
       </div>
     </div>
   );
